39ef12d770243e9a46bbffc9d6cb4c2edb4883e3
sethdorris <sethc.dorris@gmail.com>
2015-11-30 13:35:06 -0600

    Creating a jot implementation and tests

--- Jitter/Models/Jot.cs
+++ Jitter/Models/Jot.cs
@@ -6,13 +6,21 @@ using System.Web;
 
 namespace Jitter.Models
 {
-    public class Jot
+    public class Jot : IComparable
     {
-        public object Author { get; set; }
+        public JitterUser Author { get; set; }
+        [Required]
         public string Content { get; set; }
         public DateTime Date { get; set; }
         [Key]
         public int JotId { get; set; }
         public string Picture { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            Jot other_jot = obj as Jot;
+            //Multiply by -1 b/c DateTime's CompareTo Sorts Ascendingly by default.
+            return -1 * this.Date.CompareTo(other_jot.Date);   
+        }
     }
 }
